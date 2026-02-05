b6109084a31b52942c4751a1437f8d6c0f239795
cblairwhccd <christianblair@my.whccd.edu>
2026-02-04 19:55:10 -0800

    Add menu structure

--- Program.cs
+++ Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        /* Show menu options */
+        Console.WriteLine("1. Start/Continue Routine");
+        Console.WriteLine("2. View Progress");
+        Console.WriteLine("3. Exit");
+
+        /* Using a loop, repeat menu code until the user decides to exit */
+        bool running = true; /* This variable controls whether to keep running the menu */
+        while (running)
+        {
+            /* Prompt for selection */
+            Console.Write("\nEnter choice: ");
+            switch (Console.ReadLine())
+            {
+                case "1": /* Do routine step */
+                    break;
+                case "2": /* View progress */
+                    break;
+                case "3": /* Exit */
+                    /* Cannot use `break` to leave the loop here, so use a boolean variable instead */
+                    running = false; /* Next evaluation of loop condition will become false, thereby leaving the loop */
+                    break;
+            }
+        }
     }
 }
